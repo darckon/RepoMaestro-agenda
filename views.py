6b45f802033660d3053a49e82a89e04a2b73a1ef
Cristian Rojas <informatik.rojas@gmail.com>
2018-12-06 17:53:29 -0300

    Comit en modulo agenda

--- views.py
+++ views.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def dashboard(request):
+	context = {'welcome': True}
+	return render(request, 'agenda/templates/index.html', context)
